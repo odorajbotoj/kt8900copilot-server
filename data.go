aac34a51a55d57bde4e67396388cb8f02c7a6eac
odorajbotoj <odoraj_botoj@tom.com>
2026-03-19 00:30:17 +0800

    remove ctrl code about camera; add auto play

--- data.go
+++ data.go
@@ -1,31 +1,30 @@
 package main
 
 const (
-	SKIP             = 0x00
-	VERIFY           = 0x01 // s -> c
-	REFUSE           = 0x02 // s -> c
-	BUSY             = 0x03 // s -> c
-	VERIFIED         = 0x04 // s -> c
-	RX               = 0x11 // c -> s
-	RX_STOP          = 0x12 // c -> s
-	TX               = 0x13 // s -> c
-	TX_STOP          = 0x14 // s -> c
-	SET_CONF         = 0x15 // c -> s -> c
-	RESET            = 0x16 // c -> s -> c
-	PLAY             = 0x17 // c -> s -> c
-	AFSK             = 0x18 // c -> s -> c
-	S_MESSAGE        = 0x31 // c -> s -> c
-	S_S_SET_CONF     = 0x32 // c -> s -> c
-	S_E_SET_CONF     = 0x33 // c -> s -> c
-	S_E_CAM_DISABLED = 0x34 // c -> s -> c
-	S_S_PLAY         = 0x35 // c -> s -> c
-	S_E_PLAY         = 0x36 // c -> s -> c
-	S_S_AFSK         = 0x37 // c -> s -> c
-	S_E_AFSK         = 0x38 // c -> s -> c
-	FROM             = 0x51 // s -> c
-	ONLINE           = 0x52 // s -> c
-	OFFLINE          = 0x53 // s -> c
-	PCM              = 0x71 // c -> s -> c
+	SKIP         = 0x00
+	VERIFY       = 0x01 // s -> c
+	REFUSE       = 0x02 // s -> c
+	BUSY         = 0x03 // s -> c
+	VERIFIED     = 0x04 // s -> c
+	RX           = 0x11 // c -> s
+	RX_STOP      = 0x12 // c -> s
+	TX           = 0x13 // s -> c
+	TX_STOP      = 0x14 // s -> c
+	SET_CONF     = 0x15 // c -> s -> c
+	RESET        = 0x16 // c -> s -> c
+	PLAY         = 0x17 // c -> s -> c
+	AFSK         = 0x18 // c -> s -> c
+	S_MESSAGE    = 0x31 // c -> s -> c
+	S_S_SET_CONF = 0x32 // c -> s -> c
+	S_E_SET_CONF = 0x33 // c -> s -> c
+	S_S_PLAY     = 0x34 // c -> s -> c
+	S_E_PLAY     = 0x35 // c -> s -> c
+	S_S_AFSK     = 0x36 // c -> s -> c
+	S_E_AFSK     = 0x37 // c -> s -> c
+	FROM         = 0x51 // s -> c
+	ONLINE       = 0x52 // s -> c
+	OFFLINE      = 0x53 // s -> c
+	PCM          = 0x71 // c -> s -> c
 )
 
 type dataPack struct {
